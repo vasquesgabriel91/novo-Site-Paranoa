48d628b12b4cd6c9c39f671be18ef89560738aa3
Gabriel Vasques de Abreu <vasquesgabriel91@gmail.com>
2023-01-02 15:38:33 -0300

    atualizando

---
+++ javascript/main.js
@@ -0,0 +1,66 @@
+const target = document.querySelectorAll('[data-animacao]');
+const animateclass = 'animate';
+function animacaoscroll(){
+    const windowtop = window.pageYOffset + (window.innerHeight) ;
+    target.forEach(function(element){
+        if((windowtop)> element.offsetTop){
+            element.classList.add(animateclass);
+        } 
+    })
+}
+if(target.length){
+    window.addEventListener('scroll',function(){
+        animacaoscroll();
+})
+}
+
+const targetMenu = document.querySelectorAll('[data-animacao-menu]');
+const animateClassMenu = 'animate';
+function animacaoscrollmenu(){
+    const windowtop = window.pageYOffset + ((window.innerHeight * 3) /5) ;
+    targetMenu.forEach(function(element){
+        if((windowtop)> element.offsetTop){
+            element.classList.add(animateClassMenu);
+        } 
+    })
+}
+if(targetMenu.length){
+    window.addEventListener('scroll',function(){
+        animacaoscrollmenu();
+})
+}
+
+const targetProduto = document.querySelectorAll('[ data-animacaoTexoProduto]');
+const animacaoTexoProduto = 'anima';
+function animacaoTextoProdutoScroll(){
+    const windowtop = window.pageYOffset +((window.innerHeight * 6 /7));
+    targetProduto.forEach(function(elemento){
+        if((windowtop)> elemento.offsetTop){
+            elemento.classList.add(animacaoTexoProduto);
+        } 
+    })
+}
+if(targetProduto.length){
+    window.addEventListener('scroll',function(){
+        animacaoTextoProdutoScroll();
+})
+}
+
+
+const menuBtn = document.getElementById('menuBtn');
+function toggleMenu(event){
+    if (event.type === 'touchstart') event.preventDefault();
+    const menuNav = document.getElementById('menuNav');
+    menuNav.classList.toggle('active');
+    const active = menuNav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active){
+        event.currentTarget.setAttribute('aria-label','Fechar Menu')
+    }else{
+        event.currentTarget.setAttribute('aria-label','Abrir Menu')
+
+    }
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('touchstart', toggleMenu);
